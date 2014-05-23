48705821aa60597df9f277c9fee84af58206e13c
Arrvi <arrviasto@gmail.com>
2014-05-23 14:19:54 +0200

    Fixed #1

--- ColorExtractor/src/eu/arrvi/cextr/colortable/ColorTablePane.java
+++ ColorExtractor/src/eu/arrvi/cextr/colortable/ColorTablePane.java
@@ -1,6 +1,7 @@
 package eu.arrvi.cextr.colortable;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagLayout;
@@ -10,7 +11,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.border.Border;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 
 import eu.arrvi.cextr.Controller;
@@ -36,21 +39,40 @@ public class ColorTablePane extends JPanel {
 		table = new JTable(new ColorTableModel());
 		table.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
 		
-		// FIXME replace with working renderer!
 		TableColumn preview = table.getColumn("PV");
-		preview.setCellRenderer(new DefaultTableCellRenderer() {
-			@Override
-			protected void setValue(Object value) {
-				this.setLayout(new GridBagLayout());
-				JLabel label = new JLabel();
-				label.setSize(new Dimension(10, 10));
-				label.setOpaque(true);
-				label.setBackground((Color)value);
-				label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-				this.add(label);
-			}
-		});
+		preview.setCellRenderer(new ColorSampleRenderer());
+		preview.setMaxWidth(30);
 		
 		this.add(new JScrollPane(table));
+	}
+	
+	private class ColorSampleRenderer extends JPanel implements TableCellRenderer {
+	    public ColorSampleRenderer() {
+			setLayout(new GridBagLayout());
+		}
+
+		@Override
+		public Component getTableCellRendererComponent(JTable table,
+				Object value, boolean isSelected, boolean hasFocus, int row,
+				int column) {
+
+			JLabel label = new JLabel();
+			label.setOpaque(true);
+			label.setPreferredSize(new Dimension(8, 8));
+			removeAll();
+			add(label);
+			
+			Color newColor = (Color)value;
+	        label.setBackground(newColor);
+		        
+            if (isSelected) {
+                setBackground(table.getSelectionBackground());
+            } else {
+                setBackground(table.getBackground());
+            }
+            
+			return this;
+		}
+		
 	}
 }
